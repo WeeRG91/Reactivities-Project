f286b88a9c3bc1304b8246205fc6d64a68db0d31
WeeRG91 <126579703+WeeRG91@users.noreply.github.com>
2026-01-12 16:33:29 +0100

    Ajoutez des fichiers projet.

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Activity> Activities { get; set; }
+    }
+}
